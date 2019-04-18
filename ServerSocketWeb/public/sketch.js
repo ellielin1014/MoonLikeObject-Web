be26b519130ae21b2742c80f1a54940bc865acbf
Jingjie Chen <jingjiechen96@gmail.com>
2019-04-18 12:54:10 -0400

    Add files via upload

--- ServerSocketWeb/public/sketch.js
+++ ServerSocketWeb/public/sketch.js
@@ -11,6 +11,13 @@ var inputG;
 var inputB;
 var stationNum;
 
+var shape = " ";
+var size = " ";
+var AstroidName = document.getElementById('asteroidName');
+var storedName = " ";
+var stationColor = " ";
+var AstIndex;
+
 function setup() {
   // createCanvas(400, 400);
   // background(20);
@@ -30,13 +37,14 @@ function showNotif(data){
 function OnBtnClicked(){
 
   var data = {
-    name: username.value,
-    msg: msg.value,
+    name: storedName,
     r:inputR,
     g:inputG,
     b:inputB,
-    n:stationNum
+    n:stationNum,
+    a: AstIndex
   }
+
 
   //1.sending data called "new message" including data to server
   socket.emit('new message', data);
@@ -47,43 +55,169 @@ function OnBtnClicked(){
 
 //HTML chores
 
-function color2selected(){
-   btn.style.backgroundColor= "#4ECDC4";
+function goToShape(){
+  $("#startScreen").css("display", "none");
+  $(".shapeContainer").css("display", "inline-block");
+}
+
+function shape1selected(){
+   shape = "shape1";
+   
+}
+
+function shape2selected(){
+  shape = "shape2";
+}
+
+function shape3selected(){
+  shape = "shape3";
+}
+
+function goToSize(){
+  if (shape == " "){
+    alert("Please choose a shape");
+    return false;
+  } else{
+    $(".shapeContainer").css("display", "none");
+    $(".sizeContainer").css("display", "inline-block");
+  }
+  
+}
+
+function size1selected(){
+  size = "size1";
+  
+}
+
+function size2selected(){
+ size = "size2";
+}
+
+function size3selected(){
+ size = "size3";
+}
+
+function goToName(){
+  if (size == " "){
+    alert("Please choose a size");
+    return false;
+  } else{
+  $(".sizeContainer").css("display", "none");
+  $(".nameContainer").css("display", "inline-block");
+  }
+}
+
+function goToStation(){
+  
+  if (AstroidName.value.length == 0){
+    alert("Please name your asteroid");
+    return false;
+  } else {
+    storedName = AstroidName.value;
+    $(".nameContainer").css("display", "none");
+    $(".launchStationContainer").css("display", "inline-block");
+  }
+ 
+}
+
+function station1selected(){
+   //btn.style.backgroundColor= "#4ECDC4";
+   stationColor ="#4ECDC4";
+   stationNum = "1";
+   //color of asteroid trail
    inputR = "78";
    inputG = "205";
    inputB = "196";
-   stationNum = "1";
+   $("#shoot").css("background-color", stationColor);
 }
-function color3selected(){
-   btn.style.backgroundColor= "#F7FFF7"; //247,255,247
+function station2selected(){
+   //btn.style.backgroundColor= "#F7FFF7"; //247,255,247
+   stationNum = "2";
+   stationColor = "#F7FFF7";
    inputR = "247";
    inputG = "255";
    inputB = "247";
-   stationNum = "2";
+   $("#shoot").css("background-color", stationColor);
 }
-function color4selected(){
-   btn.style.backgroundColor= "#FF6B6B"; //255, 107, 107
+function station3selected(){
+   //btn.style.backgroundColor= "#FF6B6B"; //255, 107, 107
+   stationColor = "#FF6B6B";
+   stationNum = "3";
    inputR = "255";
    inputG = "107";
    inputB = "107";
-   stationNum = "3";
+   $("#shoot").css("background-color", stationColor);
 }
-function color5selected(){
-   btn.style.backgroundColor= "#FFE66D";//255,230,109
+function station4selected(){
+   //btn.style.backgroundColor= "#FFE66D";//255,230,109
+   stationColor = "#FFE66D";
+   stationNum = "4";
    inputR = "255";
    inputG = "230";
    inputB = "109";
-   stationNum = "4";
+   $("#shoot").css("background-color", stationColor);
 }
-function color6selected(){
-   btn.style.backgroundColor= "#5BC0EB";//198,61,92
+function station5selected(){
+   //btn.style.backgroundColor= "#5BC0EB";//198,61,92
+   stationColor = "#5BC0EB";
+   stationNum = "5";
    inputR = "198";
    inputG = "61";
    inputB = "92";
-   stationNum = "5";
+   $("#shoot").css("background-color", stationColor);
+}
+
+function goToShoot(){
+  if (stationColor == " "){
+    $("#shoot").attr("disabled", true);
+    alert("Please Choose a station");
+    return false;
+  } else {
+    $("#shoot").attr("disabled", false);
+    $(".launchStationContainer").css("display", "none");
+    $(".lauchPage").css("display", "inline-block");
+    $("#shoot").css("background-color", stationColor);
+    $("#triangle").css("border-bottom", "solid 130px " + stationColor);
+    $("#stationNum").text(stationNum);
+    
+    $("#insertName").html("\"" + storedName +"\"");
+    decideShapes();
+  }
 
 }
 
+function decideShapes(){
+
+   if(shape == "shape1" && size == "size1" ){
+    AstIndex = "1";  //circular small
+   }
+   if(shape == "shape1" && size == "size2" ){
+    AstIndex = "2";  //circular medium
+   }
+   if(shape == "shape1" && size == "size3" ){
+    AstIndex = "3";  //circular big
+   }
+   if(shape == "shape2" && size == "size1" ){
+    AstIndex = "4";  //irregular small
+   }
+   if(shape == "shape2" && size == "size2" ){
+    AstIndex = "5";  //irregular medium
+   }
+   if(shape == "shape2" && size == "size3" ){
+    AstIndex = "6";  //irregular big
+   }
+   if(shape == "shape3" && size == "size1" ){
+    AstIndex = "7";  //long small
+   }
+   if(shape == "shape3" && size == "size2" ){
+    AstIndex = "8";  //long medium
+   }
+   if(shape == "shape3" && size == "size3" ){
+    AstIndex = "9";  //long large
+   }
+
+}
+  
 function draw() {
 //  background(20);
 
